52100c80e90bcffb6aadb476787dd5f49bea0d6c
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-10-26 16:46:52 -0400

    jump done

--- assignments/04_flight/Assets/_Script/player.cs
+++ assignments/04_flight/Assets/_Script/player.cs
@@ -6,6 +6,10 @@ public class player : MonoBehaviour
 {
     private CharacterController characterController;
     [SerializeField] private float speed = 10f;
+    [SerializeField] Transform isGroundChecker;
+    [SerializeField] Transform Ground;
+    [SerializeField] bool isGround;
+    private float jumpForce = 200f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,24 +20,56 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GroundChecker();
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * speed * Time.deltaTime);
+        if (isGround)
+        {
+            characterController.Move(move * speed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.rotation = Quaternion.Euler(0.0f, 45f, 0.0f);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.rotation = Quaternion.Euler(0.0f, 45f, 0.0f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.rotation = Quaternion.Euler(0.0f, -45f, 0.0f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.rotation = Quaternion.Euler(0.0f, 180f, 0.0f);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.rotation = Quaternion.Euler(0.0f, 0f, 0.0f);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                characterController.Move(Vector3.up * jumpForce * Time.deltaTime);
+            }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        if (!isGround)
         {
-            transform.rotation = Quaternion.Euler(0.0f, -45f, 0.0f);
+            characterController.Move(Vector3.down * 9.8f * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+
+    }
+
+    private bool GroundChecker()
+    {
+        float isGroundCheckerY = isGroundChecker.transform.position.y;
+        float GroundY = Ground.transform.position.y;
+        float fromGround = isGroundCheckerY - GroundY;
+        if (fromGround <= 0.1)
         {
-            transform.rotation = Quaternion.Euler(0.0f, 180f, 0.0f);
+           isGround = true;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else
         {
-            transform.rotation = Quaternion.Euler(0.0f, 0f, 0.0f);
+            isGround = false;
         }
+
+        return isGround;
     }
 }
