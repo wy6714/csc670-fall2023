8a1bf01f72ec938ebd38e3ae6dcfddd98b68eb46
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-11-23 10:08:06 -0500

    mouse hover and exit highlight selected block

--- assignments/05_units/Assets/_Script/Grid.cs
+++ assignments/05_units/Assets/_Script/Grid.cs
@@ -2,25 +2,43 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public GameObject normalBlock; 
+    public GameObject normalBlock;
+
+    public Sprite hoverSprite;
+    public Sprite normalSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
-        
+
         if (Input.GetMouseButtonDown(0))
         {
-            
+            //mouse click place block
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            
             if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
             {
-                
                 Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y, 10f);
                 Instantiate(normalBlock, spawnPosition, Quaternion.identity);
                 Debug.Log("collide");
             }
         }
+    }
+
+    private void OnMouseOver()//mouse hover -> hoverSprite
+    {
+        spriteRenderer.sprite = hoverSprite;
+    }
+
+    private void OnMouseExit()//mouse exit -> back normal
+    {
+        spriteRenderer.sprite = normalSprite;
     }
 }
