7d8eaf7e4d4f13dbea1ae81f00d8741fc02b28d9
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-11-23 19:35:27 -0500

    player controller

---
+++ assignments/05_units/Assets/_Script/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float horizontal;
+    private float speed = 5f;
+
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal <0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
